f654989b6c86b0151eb6ea8e88e69b4370a351b5
hykimyong <hykimyong@naver.com>
2023-06-07 23:54:23 +0900

    footer추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const MainPage:React.FC =() => {
   return (
     <div>
       <Header/>
+      <Footer/>
     </div>
   )
 }
