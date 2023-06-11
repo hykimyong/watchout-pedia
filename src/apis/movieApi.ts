4257ef22fb533439672f167eb6add40442b77bd8
hykimyong <hykimyong@naver.com>
2023-06-11 16:58:38 +0900

    리스트 생성

--- src/apis/movieApi.ts
+++ src/apis/movieApi.ts
@@ -1,6 +1,6 @@
-import axiosInstance from "./index";
+import axiosInstance from './index';
 
-export const lastestApi = () => axiosInstance.get('/movie/latest');
+export const latestApi = () => axiosInstance.get('/movie/latest');
 
 export const upcomingApi = () => axiosInstance.get('/movie/upcoming');
 
@@ -12,6 +12,6 @@ export const popularApi = () => axiosInstance.get('/movie/popular');
 
 export const detailApi = (movieId: string) => axiosInstance.get(`/movie/${movieId}`);
 
-export const similarApi = (movieId: string) => axiosInstance.get(`/movie/${movieId}/similar`);
+export const similarApi = (id: string) => axiosInstance.get(`/movie/${id}/similar`);
 
 export const searchApi = (query: string) => axiosInstance.get(`/search/movie?query=${query}`);
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'reset-css';
+import './index.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <QueryClientProvider client = {queryClient}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
