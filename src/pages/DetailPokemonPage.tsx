606a1a3f165bf0f861d72fbceb602238b3488d47
hykimyong <hykimyong@naver.com>
2023-07-14 21:17:52 +0900

    상세페이지,탭 추가

--- src/pages/DetailPokemonPage.tsx
+++ src/pages/DetailPokemonPage.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Tabs from '../components/Tabs';
 
 type Params = {
     id: string;
 }
 
+type Tab = 'about' | 'stats' | 'evolution';
+
 const DetailPokemonPage: React.FC = () =>{
     const {id} = useParams<Params>();
+    const [selecteTab, setSelectedTab] = useState<Tab>('about');
+
+    const handleClick = (tab:Tab)=>{
+        setSelectedTab(tab);
+    }
 
     return(
-        <div>DetailPokemonPage  id : {id}</div>
+        <div>
+            <Tabs tab={selecteTab} onClick={handleClick} color={{name:"red",url:""}}/>
+        </div>
     )
 }
 export default DetailPokemonPage;
